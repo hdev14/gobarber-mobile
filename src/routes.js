44510e0b39e95d4724671b1d59afe8f1e79bf53c
HermersonDev <ne.hermerson@gmail.com>
2020-06-08 20:59:33 -0300

    :heavy_plus_sign: :construction: add react-navigation/bottom-tabs and create dashboard screen

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import SignIn from './screens/SignIn';
 import SignOut from './screens/SignOut';
+import Dashboard from './screens/Dashboard';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Routes() {
-  return (
+  const signed = useSelector((s) => s.auth.signed);
+
+  return signed ? (
+    <Tab.Navigator>
+      <Tab.Screen name="dashboard" component={Dashboard} />
+    </Tab.Navigator>
+  ) : (
     <Stack.Navigator headerMode="none">
       <Stack.Screen name="signin" component={SignIn} />
       <Stack.Screen name="signout" component={SignOut} />
