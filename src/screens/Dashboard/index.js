81183947287dff45b42b9becdb188877716d42fd
HermersonDev <ne.hermerson@gmail.com>
2020-06-22 21:35:06 -0300

    :construction: add useFocusEffect

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
+import {useFocusEffect} from '@react-navigation/native';
 
 import api from '../../services/api';
 
@@ -10,14 +11,16 @@ import {DashboardShape, AppointmentList} from './styles';
 export default function Dashboard() {
   const [appointments, setAppointments] = useState([]);
 
-  useEffect(() => {
-    async function fetchAppointments() {
-      const response = await api.get('/appointments');
-      setAppointments(response.data);
-    }
+  async function fetchAppointments() {
+    const response = await api.get('/appointments');
+    setAppointments(response.data);
+  }
 
-    fetchAppointments();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      fetchAppointments();
+    }, []),
+  );
 
   async function handleCancel(id) {
     const response = await api.delete(`/appointments/${id}`);
